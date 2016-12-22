54e87ccfb44a05d64fef16cb0395f8e7b2db079f
tomii <g244103@outlook.com>
2016-12-22 20:23:23 +0530

    level change

--- scripts/stagegenerator.js
+++ scripts/stagegenerator.js
@@ -2,7 +2,7 @@ function generateStage(n){
 
 //  var num = (Math.floor((n-1) % 3) + 1)*2;
   var num = (Math.floor((n-1) % 10) + 1)*2;
-  var speed = Math.floor((n / 10)) + 0.5;
+  var speed = Math.floor((n / 10))*0.2 + 0.5;
   var time = 10;
   var message = "stage " + n;
   return [[num, speed],time,message];
