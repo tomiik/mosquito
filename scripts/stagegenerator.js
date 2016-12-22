4a0114c6f50262092694e0387650a2c7559b8e50
tomii <g244103@outlook.com>
2016-12-22 15:44:17 +0530

    stage generate

--- scripts/stagegenerator.js
+++ scripts/stagegenerator.js
@@ -1,8 +1,8 @@
 function generateStage(n){
 
-  var num = (Math.floor(n % 4) + 1) * 3;
-  var speed = Math.floor((n / 5)) * 0.2 + 0.2;
-  var time = 10 + Math.floor((n/3));
+  var num = (Math.floor((n-1) % 3) + 1);
+  var speed = Math.floor((n / 3)) * 0.2 + 0.2;
+  var time = 10;
   var message = "stage " + n;
   return [[num, speed],time,message];
 }
